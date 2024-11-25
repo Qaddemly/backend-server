cbd67e9b8e74e63ca109376f659e985c4d1334f5
Abdo Khattab <abdulrhmankhattab11@gmail.com>
2024-11-25 22:29:17 +0200

    Added Database Connection Configration

--- src/middlewares/error.middleWare.ts
+++ src/middlewares/error.middleWare.ts
@@ -1,40 +1,41 @@
-import { Request, Response, NextFunction } from "express";
-import AppError from "../utils/appError";
+import { NextFunction, Request, Response } from 'express';
+import AppError from '../utils/appError';
+
 const devError = (err: AppError, res: Response) => {
-  res.status(err.statusCode!).json({
-    status: err.status,
-    name: err.name,
-    message: err.message,
-    details: err.stack,
-  });
+    res.status(err.statusCode!).json({
+        status: err.status,
+        name: err.name,
+        message: err.message,
+        details: err.stack,
+    });
 };
 const sendValidationError = (err: AppError, res: Response) => {
-  res.status(err.statusCode!).json({
-    status: err.status,
-    name: err.name,
-    message: err.message,
-    details: err.validatorErrors,
-  });
+    res.status(err.statusCode!).json({
+        status: err.status,
+        name: err.name,
+        message: err.message,
+        details: err.validatorErrors,
+    });
 };
 const sendErrorProduction = (err: AppError, res: Response) => {
-  res.status(err.statusCode!).json({
-    status: err.status,
-    name: err.name,
-    message: err.message,
-  });
+    res.status(err.statusCode!).json({
+        status: err.status,
+        name: err.name,
+        message: err.message,
+    });
 };
 export const globalErrorHandler = (
-  err: AppError,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    err: AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
 ) => {
-  err.statusCode = err.statusCode || 500;
-  if (err.message === "validationErrors") {
-    sendValidationError(err, res);
-  } else if (process.env.NODE_ENV === "development") {
-    devError(err, res);
-  } else {
-    sendErrorProduction(err, res);
-  }
+    err.statusCode = err.statusCode || 500;
+    if (err.message === 'validationErrors') {
+        sendValidationError(err, res);
+    } else if (process.env.NODE_ENV === 'development') {
+        devError(err, res);
+    } else {
+        sendErrorProduction(err, res);
+    }
 };
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
 import app from './app';
+import databaseConnect from './config/database.connection';
 
 dotenv.config();
 
 // database connection
 
 const port = process.env.PORT || 3000;
+
+databaseConnect();
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
