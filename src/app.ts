f5947ec7235f1e41013394c274f2771e09c86b90
Abdo Khattab <abdulrhmankhattab11@gmail.com>
2024-11-26 15:28:55 +0200

    Deleted Un-nessary lines

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,9 @@
 import express from 'express';
 import { globalErrorHandler } from './middlewares/error.middleWare';
-import catchAsync from 'express-async-handler';
-import User from './models/userModel';
 
 const app = express();
 app.use(express.json());
 
 app.use(globalErrorHandler);
-
-app.post(
-    '/',
-    catchAsync(async (req, res) => {
-        const user = new User(req.body);
-        await user.save();
-        res.json(user);
-    }),
-);
 
 export default app;
