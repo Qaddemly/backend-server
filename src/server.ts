d895887eb34293b0f3d019b26f670cc71c99f803
eslamso <eslam.11359@stemalex.moe.edu.eg>
2024-11-26 16:15:00 +0200

    done

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import "colors";
 import dotenv from "dotenv";
+import connectDB from "./config/dbConnection";
 dotenv.config();
 import app from "./app";
-
+const dbUrl = process.env.DATA_BASE!;
+connectDB(dbUrl);
 // database connection
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`.green);
 });
