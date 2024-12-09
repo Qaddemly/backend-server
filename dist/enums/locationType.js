508da1471b53cf3140018c8cee770f93ceb8016f
eslamso <eslam.11359@stemalex.moe.edu.eg>
2024-12-09 16:21:38 +0200

    ll

--- dist/enums/locationType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LocationType = void 0;
-var LocationType;
-(function (LocationType) {
-    LocationType["Remote"] = "Remote";
-    LocationType["Onsite"] = "Onsite";
-    LocationType["Hybrid"] = "Hybrid";
-})(LocationType || (exports.LocationType = LocationType = {}));
