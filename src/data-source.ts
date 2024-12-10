eb13d44b483840842db0f4cb11ec2b6fe5216a45
eslamso <eslam.11359@stemalex.moe.edu.eg>
2024-12-11 00:58:34 +0200

    woooooooow

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: 'localhost',
-    port: 5432,
+    port: 5433,
     username: 'postgres',
     password: 'test',
     database: 'test',
