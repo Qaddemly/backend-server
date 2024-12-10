29fb2642927e95d38ce40a152114dc46c8b82c49
Abdo Khattab <abdulrhmankhattab11@gmail.com>
2024-12-11 00:47:52 +0200

    Added Controller description

--- src/controllers/businessController.ts
+++ src/controllers/businessController.ts
@@ -3,9 +3,18 @@ import catchAsync from 'express-async-handler';
 import { CreateBusinessDto } from '../dtos/businessDto';
 
 import * as businessServices from './../services/businessServices';
-import exp from 'constants';
 import { getBusinessQueryParams } from '../types/types';
 
+/**
+ *
+ * Creation of a business page
+ *
+ * This function creates a business profile and mark the user as the owner of the business,
+ * and returns the business profile created.
+ *
+ * TODO: Add image upload for the business profile
+ *
+ * */
 export const createBusiness = catchAsync(
     async (req: Request<{}, {}, CreateBusinessDto>, res: Response) => {
         const business = await businessServices.createBusiness(req.body, 0);
@@ -17,13 +26,27 @@ export const createBusiness = catchAsync(
     },
 );
 
-export const getBusinesses = catchAsync(
+/**
+ *
+ * Search on business profiles by prefix of the business name,
+ * and returns 20 business profiles that match the search.
+ *
+ *
+ * TODO: talk with frontend about what they need to display in the search results
+ * Each business profile contains : companyName, logo, id
+ *
+ * */
+export const searchBusinessByName = catchAsync(
     async (
         req: Request<{}, {}, {}, getBusinessQueryParams>,
         res: Response,
     ) => {},
 );
 
+/**
+ * Get a detailed business profile by the business name
+ * TODO: talk with frontend about what they need to display in the business profile
+ * */
 export const getBusinessById = catchAsync(
     async (req: Request, res: Response) => {},
 );
--- src/entity/Account.ts
+++ src/entity/Account.ts
@@ -1,5 +1,5 @@
-
 import {
+    Column,
     Entity,
     JoinTable,
     ManyToMany,
