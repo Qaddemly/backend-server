c10c2d7237da94b57c05af537793b946a428fe66
Abdo Khattab <abdulrhmankhattab11@gmail.com>
2024-12-11 00:03:07 +0200

    Added Outline of busniess controller

--- src/controllers/businessController.ts
+++ src/controllers/businessController.ts
@@ -3,6 +3,8 @@ import catchAsync from 'express-async-handler';
 import { CreateBusinessDto } from '../dtos/businessDto';
 
 import * as businessServices from './../services/businessServices';
+import exp from 'constants';
+import { getBusinessQueryParams } from '../types/types';
 
 export const createBusiness = catchAsync(
     async (req: Request<{}, {}, CreateBusinessDto>, res: Response) => {
@@ -13,4 +15,21 @@ export const createBusiness = catchAsync(
             business,
         });
     },
+);
+
+export const getBusinesses = catchAsync(
+    async (
+        req: Request<{}, {}, {}, getBusinessQueryParams>,
+        res: Response,
+    ) => {},
+);
+
+export const getBusinessById = catchAsync(
+    async (req: Request, res: Response) => {},
+);
+export const updateBusiness = catchAsync(
+    async (req: Request, res: Response) => {},
+);
+export const deleteBusiness = catchAsync(
+    async (req: Request, res: Response) => {},
 );
