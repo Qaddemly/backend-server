5354fbc58f852abeaf2d32a40e8d5e980859ba88
Abdo Khattab <abdulrhmankhattab11@gmail.com>
2024-12-15 19:10:16 +0200

    fixed error in jobs

--- src/Repository/jobRepository.ts
+++ src/Repository/jobRepository.ts
@@ -4,47 +4,41 @@ import { Job } from '../entity/Job';
 
 class JobRepositoryClass extends Repository<Job> {
     async getSixJobsOfBusiness(businessId: number) {
-        return await this.query(
-            'SELECT j.description as job_description,' +
-                ' j.id as job_id,' +
-                ' j.title as job_title,' +
-                ' j.location as job_location,' +
-                ' j.location_type as job_location_type,' +
-                ' j.skills as job_skills,' +
-                ' j.salary as job_salary,' +
-                ' j.employee_type as job_employee_type,' +
-                ' j.keywords as job_keywords,' +
-                ' j.experience as job_experience,' +
-                ' j.created_at as job_create_at,' +
-                ' b.id as business_id,' +
-                ' b.name as business_name,' +
-                ' b.logo as business_logo,' +
-                ' b.industry as business_industry' +
-                ' FROM job as j JOIN business as b ON j.business_id = $1 LIMIT 6',
-            [businessId],
-        );
+        return this
+            .query(`SELECT j.description as job_description,j.id as job_id,
+                        j.title as job_title,
+                        j.location as job_location,
+                        j.location_type as job_location_type,
+                        j.skills as job_skills,
+                        j.salary as job_salary,
+                        j.employee_type as job_employee_type,
+                        j.keywords as job_keywords,
+                        j.experience as job_experience,
+                        j.created_at as job_create_at,
+                        b.id as business_id,
+                        b.name as business_name,
+                        b.logo as business_logo,
+                        b.industry as business_industry
+                        FROM job as j JOIN business as b ON (j.business_id = b.id AND b.id = ${businessId}) LIMIT 6`);
     }
 
     async getAllJobsOfBusiness(businessId: number) {
-        return this.query(
-            'SELECT j.description as job_description,' +
-                ' j.id as job_id,' +
-                ' j.title as job_title,' +
-                ' j.location as job_location,' +
-                ' j.location_type as job_location_type,' +
-                ' j.skills as job_skills,' +
-                ' j.salary as job_salary,' +
-                ' j.employee_type as job_employee_type,' +
-                ' j.keywords as job_keywords,' +
-                ' j.experience as job_experience,' +
-                ' j.created_at as job_create_at,' +
-                ' b.id as business_id,' +
-                ' b.name as business_name,' +
-                ' b.logo as business_logo,' +
-                ' b.industry as business_industry' +
-                ' FROM job as j JOIN business as b ON j.business_id = $1',
-            [businessId],
-        );
+        return this
+            .query(`SELECT j.description as job_description,j.id as job_id,
+                        j.title as job_title,
+                        j.location as job_location,
+                        j.location_type as job_location_type,
+                        j.skills as job_skills,
+                        j.salary as job_salary,
+                        j.employee_type as job_employee_type,
+                        j.keywords as job_keywords,
+                        j.experience as job_experience,
+                        j.created_at as job_create_at,
+                        b.id as business_id,
+                        b.name as business_name,
+                        b.logo as business_logo,
+                        b.industry as business_industry
+                        FROM job as j JOIN business as b ON (j.business_id = b.id AND b.id = ${businessId})`);
     }
 }
 
