3d36373faf3c51a6992dad91f0b7124bbf802024
Abdo Khattab <abdulrhmankhattab11@gmail.com>
2025-02-24 00:42:27 +0200

    added redis client

--- src/server.ts
+++ src/server.ts
@@ -5,31 +5,24 @@ import app from './app';
 import databaseConnect from './config/database.connection';
 import { AppDataSource } from './data-source';
 import { Logger } from './utils/logger';
+import { redisClient } from './config/redis';
 
 const port = process.env.PORT || 3000;
+
+redisClient
+    .connect()
+    .then(() => {
+        Logger.info('Redis connected successfully');
+    })
+    .catch((error) => {
+        Logger.error('Redis connection failed', error);
+    });
 
 databaseConnect();
 
 AppDataSource.initialize()
     .then(async () => {
         Logger.info('Postgres Database connected successfully');
-        // const password = await hashingPassword('12345678');
-        // const account = new Account();
-        // account.first_name = 'abdo';
-        // account.last_name = 'khattab';
-        // account.email = 'abdo@mailsac.com';
-        // account.password = password;
-        // account.date_of_birth = new Date('1997-09-09');
-        // account.profile_picture = 'https://www.google.com';
-        //
-        // const address = new Address();
-        // address.country = Country.Algeria;
-        // address.city = 'Cairo';
-        //
-        // account.address = address;
-
-        // account.resume = 'https://www.google.com';
-        // await AccountRepo.save(account);
     })
     .catch((error) => {
         Logger.error('Postgres Database connection failed', error);
